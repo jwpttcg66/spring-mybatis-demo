adefcf6d8bca2d6c827f4d0f376d4c67d4c7e5b0
jiangwenping <jwpttcg66>
2017-03-06 14:36:29 +0800

    init

--- src/main/java/com/snowcattle/demo/mapper/OrderMapper.java
+++ src/main/java/com/snowcattle/demo/mapper/OrderMapper.java
@@ -1,11 +1,9 @@
 package com.snowcattle.demo.mapper;
 
 import com.snowcattle.demo.entity.Order;
-import org.springframework.stereotype.Repository;
 
 /**
  */
-@Repository
 public interface OrderMapper{
     public int insertOrder(Order address);
 }
